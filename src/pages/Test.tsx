3bd09cc1070044a9ffe1faeecbd2767779fc5280
kamil hussain <kamil467@gmail.com>
2025-02-16 19:32:05 +0300

    desktop video width issue resolved

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -12,7 +12,7 @@ const MasonryGrid = () => {
     { id: 6, cols: 2, rows: 2, background: '#D4A5A5', type: 'image', image: 'https://picsum.photos/id/239/200/300' },
     { id: 7, cols: 1, rows: 2, background: '#77C9D4',  },
     { id: 8, cols: 2, rows: 3, background: '#FF9999' , type: 'image', image: 'https://picsum.photos/id/241/200/300' },
-    { id: 9, cols: 1, rows: 3, background: '#B0E0E6', type: 'vimeo', videoId: '1057103201'  },
+    { id: 9, cols: 1, rows: 3, background: '#e5e7eb', type: 'vimeo', videoId: '1057103201'  },
     { id: 10, cols: 1, rows: 3, background: '#FF6B6B' },
     { id: 11, cols: 2, rows: 2, background: '#4ECDC4' },
     { id:12, cols: 1, rows: 2, background: '#45B7D1' },
