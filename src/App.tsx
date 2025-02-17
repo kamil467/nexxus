285e9cf33b8462bcbc522c4476b0c8c8cb5ac6f3
kamil hussain <kamil467@gmail.com>
2025-02-17 20:27:27 +0300

    contact button added

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
           <Route path="/work-details" element={<WorkDetails />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/work" element={<Test />} />
+        {/* <Route path="/work" element={<Test />} />*/}  
         </Routes>
       </main>
       <Footer />
